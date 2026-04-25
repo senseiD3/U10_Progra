3464f7f6e154e0fcb098d30b1644eeafa10881db
David Miguel <tuemail@ejemplo.com>
2026-04-25 17:30:39 +0200

    "a03 terminado"

--- a03/A03.java
+++ a03/A03.java
@@ -3,7 +3,6 @@ package a03;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Random;
-import java.util.Iterator;
 
 public class A03 {
     public static void main (String[] args){
